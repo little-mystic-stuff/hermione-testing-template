4511c87034f3b8d4d432d21eb1c939827fe7b058
Little-Mystic-Stuff <little.mystic.stuff@gmail.com>
2021-08-16 15:47:56 +0300

    openPage fix

--- commands/openPage.js
+++ commands/openPage.js
@@ -1,3 +1,5 @@
-module.exports = async function (urlComponent = '/') {
-  await this.url(urlComponent);
+module.exports = async function (urlComponent) {
+  console.log(this.options.baseUrl);
+  const url = urlComponent === undefined ? this.options.baseUrl : urlComponent;
+  await this.url(url);
 };
--- tests/desktop/tests.js
+++ tests/desktop/tests.js
@@ -3,7 +3,7 @@ const should = require('chai').should();
 describe('Redirects.', async () => {
   it('Default', async function () {
     await this.browser.openPage();
-    const title = await this.browser.$('#readme h2').getText();
+    const title = await this.browser.$('#readme .markdown-body h2').getText();
     title.should.equal('Hermione testing template');
   });
 });
